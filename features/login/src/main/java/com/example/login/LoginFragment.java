f06cfc2ed2202f93a99f8e7c44b12778ae30e9a0
Vangelis <axokid318@gmail.com>
2021-03-17 17:42:34 +0000

    Added register screen and integrated it with login navigation. Added google-services.json in session module

--- features/login/src/main/java/com/example/login/LoginFragment.java
+++ features/login/src/main/java/com/example/login/LoginFragment.java
@@ -10,21 +10,34 @@ import androidx.navigation.Navigation;
 
 /**
  * A simple {@link Fragment} subclass.
- * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
-
-//    private EditText u
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        // Set Listeners for clickable items
+        setOnClickListeners(view);
+
+        return view;
+    }
+
+
+    private void setOnClickListeners(View view){
+        // Set listener for sign-in click
         view.findViewById(R.id.button_login_signin).setOnClickListener(v -> {
             int action = R.id.action_loginFragment_to_avatarFragment; // action is NavDirections class normally
             Navigation.findNavController(v).navigate(action);
         });
-        return view;
+
+        // Set listener for register click
+        view.findViewById(R.id.register_link).setOnClickListener(v -> {
+            int action = R.id.action_loginFragment_to_registerFragment;
+            Navigation.findNavController(v).navigate(action);
+        });
+
     }
 }
---
+++ features/login/src/main/java/com/example/login/RegisterFragment.java
@@ -0,0 +1,25 @@
+package com.example.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        return view;
+    }
+}
