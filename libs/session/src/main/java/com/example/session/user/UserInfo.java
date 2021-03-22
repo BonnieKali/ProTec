41820282d7998ce112645c1843edd76fc10d8b46
Vangelis <axokid318@gmail.com>
2021-03-22 14:48:42 +0000

    Added animations and prettified Login

--- libs/session/src/main/java/com/example/session/user/UserInfo.java
+++ libs/session/src/main/java/com/example/session/user/UserInfo.java
@@ -27,6 +27,22 @@ public class UserInfo {
     }
 
 
+    /**
+     * Returns the part of the email to the left of the '@' symbol.
+     *
+     * @return String username
+     */
+    public String getUserName(){
+        int at_index = email.indexOf("@");
+
+        if (at_index < 1){
+            return email;
+        }else{
+            return email.substring(0, at_index);
+        }
+    }
+
+
     @Override
     public String toString() {
         return "UserInfo{" +
