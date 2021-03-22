f6d082c21f708f13d4975059537eb46945b1ff7c
axo318 <43636591+axo318@users.noreply.github.com>
2021-03-22 14:50:27 +0000

    Added animations and prettified Login (#2)

--- features/login/src/main/java/com/example/login/AvatarFragment.java
+++ features/login/src/main/java/com/example/login/AvatarFragment.java
@@ -1,7 +1,9 @@
 package com.example.login;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.actions.Actions;
 import com.example.session.Session;
+import com.example.threads.BackgroundPool;
+import com.example.threads.OnTaskCompleteCallback;
+import com.example.threads.RunnableTask;
+import com.example.threads.TaskResult;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,8 +26,12 @@ import com.example.session.Session;
 public class AvatarFragment extends Fragment {
     private static final String TAG = "AvatarFragment";
 
+    // Specifies duration for the life of the current fragment
+    private final float AVATAR_DURATION_SECONDS = 2.0f;
+
     // UI Variables
     private TextView imageCaption;
+    private TextView hello_email;
 
     // Logic
     private Session session;
@@ -37,11 +47,32 @@ public class AvatarFragment extends Fragment {
 
         // Retrieve UI
         imageCaption = view.findViewById(R.id.avatar_caption);
+        hello_email = view.findViewById(R.id.avatar_email);
 
         // Log user details
         Log.d(TAG, "User object is: "+session.getUser().toString());
-        imageCaption.setText(session.getUser().userInfo.email);
 
+        // Set UI text
+        imageCaption.setText(session.getUser().userInfo.email);
+        hello_email.setText(session.getUser().userInfo.getUserName());
+
+        // Set action listener
+        setListeners(view);
+
+        return view;
+    }
+
+
+    /**
+     * Sets listeners
+     *
+     * @param view Current inflated view
+     */
+    private void setListeners(View view){
+
+        // This is used when we want the user to click the button to go to dashboard
+        // Currently this happens automatically after AVATAR_DURATION_SECONDS
+        /*
         // Set listener to button. This takes the user to the dashboard.
         view.findViewById(R.id.button_login_toapp).setOnClickListener(v -> {
             Activity act = getActivity();
@@ -49,6 +80,39 @@ public class AvatarFragment extends Fragment {
                 act.startActivity(Actions.openDashboardIntent(act));
             }
         });
-        return view;
+        */
+
+        // Go to next target after N seconds if the button is not clicked
+        setGoTimer();
     }
+
+
+    /**
+     * Sets timer which takes user to dashboard
+     */
+    private void setGoTimer() {
+
+        // Create background task which waits AVATAR_DURATION_SECONDS and sends an result
+        RunnableTask task = () -> {
+            SystemClock.sleep((long)(AVATAR_DURATION_SECONDS*1000));
+            return new TaskResult<>(null);
+        };
+
+        // Attach task and go to dashboard upon completion
+        BackgroundPool.attachTask(task, taskResult -> toDashboard());
+    }
+
+
+    /**
+     * Takes user to dashboard, clearing the view stack
+     */
+    private void toDashboard(){
+        Activity act = getActivity();
+        if(act != null){
+            Intent intent = Actions.openDashboardIntent(act);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            act.startActivity(intent);
+        }
+    }
+
 }
