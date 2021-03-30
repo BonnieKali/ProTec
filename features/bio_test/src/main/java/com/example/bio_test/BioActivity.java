5ecf362acbad054f186e66997deb032d04b3ae1e
theColorfulRainbow <30596510+theColorfulRainbow@users.noreply.github.com>
2021-03-30 13:19:31 +0100

    Fall Detector added in Bio Features (#21)
    * can get accelerometer values
    
    * first working version of fall detection

--- features/bio_test/src/main/java/com/example/bio_test/BioActivity.java
+++ features/bio_test/src/main/java/com/example/bio_test/BioActivity.java
@@ -1,28 +1,32 @@
 package com.example.bio_test;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class BioActivity extends AppCompatActivity {
+
+    FallDetector fall_detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bio);
-//        showFragment(new BioTestButtonFragment());
-        showFragment(new BiomarkerTests());
+        setContentView(R.layout.activity_bio);                  // content view
+//        showFragment(new BiomarkerTests());                     // show fragment
+        showFragment(new FallDetectorFragment());
     }
 
 
     private void showFragment(Fragment fragment){
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_biomarker, fragment).commit();
+
     }
 
+    private void showFragmentFallDetector(){
+        FallDetectorFragment fragment = new FallDetectorFragment();
+        Bundle bundle = new Bundle();
+        fragment.setArguments(bundle);
+    }
 
 }
